69fea898b258b3eeedde78e84919a64b576d5cd3
yaxi1984 <yaxi1984@5c960c4a-2e93-11df-9ac8-ff20597b2c9b>
2010-04-12 14:45:44 +0000

    User env.bat to replace env.lnk;

---
+++ trunk/common_codebase/source/env.bat
@@ -0,0 +1,21 @@
+:: ############################################################################
+::  COPYRIGHT:
+::    Copyright (c)  2005-2010   Source Photonics Inc.    All rights reserved.
+::
+::    This is unpublished proprietary source code of Source Photonics Inc.
+::    The copyright notice above does not evidence any actual or intended
+::    publication of such source code.
+::
+::  DESCRIPTION:
+::    project environment to be build.
+::
+::  HISTORY:
+::    2009.4.10        Panda.Xiong         Create
+::
+:: ############################################################################
+
+:: close command shell echo
+@echo off
+
+cmd /k ".\make\env.bat"
+
