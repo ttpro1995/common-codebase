d898a11639b2c71082a2bca961465a5ff20ed4a7
yaxi1984 <yaxi1984@5c960c4a-2e93-11df-9ac8-ff20597b2c9b>
2010-07-06 07:08:52 +0000

    Add Register Analyzer Tool.

---
+++ RegAnalyzer/src/Main.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace RegAnalyzer
+{
+    static class Startup
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmRegAnalyzer());
+        }
+    }
+}
